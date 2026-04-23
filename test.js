ff080154905cf12d9edf173bcab5db371f441d98
michael <fenotianamichael81@gmail.com>
2026-04-23 12:02:58 +0000

    Ajout contribution micka

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
+<<<<<<< HEAD
 require("./mahery.js")
+=======
+
+console.log("=== TEST DEVOPS ===");
+
+function addition(a, b) {
+    return a + b;
+}
+
+let result = addition(2, 2);
+
+if (result === 4) {
+    console.log("Test OK : " + result);
+} else {
+    console.log("Test FAILED");
+}
+
+require("./student_RAKOTO_Michael");
+>>>>>>> c202b6d (Ajout contribution micka)

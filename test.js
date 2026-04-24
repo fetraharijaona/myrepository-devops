fb5aeaec36021ea58d64a349715f2d0c3018d19f
Shadow-hub69 <stephaneraf12@gmail.com>
2026-04-24 22:31:02 +0300

    Ajout contribution STEPHANE

--- test.js
+++ test.js
@@ -5,5 +5,6 @@ require("./student_RASOLONIRINA_Andry_Hasimbola.js");
 require("./student_RAKOTO_Michael");
 require("./andy.js");
 require("./mahery.js");
-require("./tinah.js")
+require("./tinah.js");
+require("./stephane.js");
 

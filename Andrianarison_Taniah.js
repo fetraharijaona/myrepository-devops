34cbc74f7ca297b29922d9fb287e3b6cea1f7216
Andrianarison Taniah <taniahandrianarison@gmail.com>
2026-04-30 13:15:43 +0200

    ajout contribution

---
+++ Andrianarison_Taniah.js
@@ -0,0 +1,6 @@
+function presentation(){
+    console.log("Nom : Andrianarison");
+    console.log("Prénom : Taniah");
+}
+
+presentation();
--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 
 console.log("=== TEST DEVOPS ===");
 
-require("./student_RASOLONIRINA_Andry_Hasimbola.js");
-require("./student_RAKOTO_Michael");
 require("./andy.js");
 require("./mahery.js");
+require("./student_RAKOTO_Michael.js");
 require("./tinah.js");
-require("./stephane.js");
-
+require("./Andrianarison_Taniah.js");
